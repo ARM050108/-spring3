cc2cd422cf7f351612f522a9cac18badf4b35d22
Alex <amirzoev7@yahoo.com>
2024-11-26 14:36:38 +0300

    Updated models: Duck5, Egg6, Needle7, Rabbit4

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,21 @@
 package koschei.models;
 
+import koschei.KoscheiTheDeathless;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+
+    private final KoscheiTheDeathless koschei;
+
+    @Autowired
+    public Needle7(KoscheiTheDeathless koschei) {
+        this.koschei = koschei;
+    }
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :(";
     }
 }
